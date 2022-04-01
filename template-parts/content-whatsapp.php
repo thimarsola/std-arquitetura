c31a4dfa81826ed1426dcfb1fae36e170f0654c9
Marsola <tmarsola94@gmail.com>
2022-04-01 15:36:16 -0300

    modified whatsapp button

--- template-parts/content-whatsapp.php
+++ template-parts/content-whatsapp.php
@@ -1,11 +1,26 @@
 <div id="whatsapp" class="whatsapp__button">
 
-        <i class="stdarquitetura-whatsapp"></i>
+    <i class="stdarquitetura-whatsapp"></i>
 
-        <!-- container -->
-        <div class="whatsapp__button__container">
-            <?= whatsapp(formatPhone(CONTACT['whatsapp']), CONTACT['whatsappMessage'], 'Clientes', 'btn'); ?>
-            <?= whatsapp(formatPhone(CONTACT['whatsapp']), 'Olá STD Arquitetura, sou um fornecedor e gostaria de apresentar meu produto e/ou serviço', 'Fornecedores', 'btn'); ?>
+    <!-- container -->
+    <div class="whatsapp__button__container">
+        <!-- client -->
+        <div class="whatsapp__button__container__group">
+            <?= whatsapp(formatPhone(CONTACT['whatsapp']), CONTACT['whatsappMessage'], 'Clientes', null); ?>
+            <span>
+                <i class="stdarquitetura-whatsapp"></i>
+            </span>
         </div>
-        <!-- end of container -->
+        <!-- end of client -->
+
+        <!-- provider -->
+        <div class="whatsapp__button__container__group">
+            <?= whatsapp(formatPhone(CONTACT['whatsapp']), 'Olá STD Arquitetura, sou um fornecedor e gostaria de apresentar meu produto e/ou serviço', 'Fornecedores', null); ?>
+            <span>
+                <i class="stdarquitetura-whatsapp"></i>
+            </span>
+        </div>
+        <!-- end of provider -->
+    </div>
+    <!-- end of container -->
 </div>
